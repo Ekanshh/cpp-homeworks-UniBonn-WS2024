4b92491f5774a91466c5a35fffef4c4e526982ee
Ekansh Sharma <ekanshh.sharma@gmail.com>
2023-11-05 12:18:43 +0100

    [homework_1] Update commands script for task_1

--- homework_1/task_1/commands.sh
+++ homework_1/task_1/commands.sh
@@ -5,4 +5,4 @@ grep -E "dolor|dalor" data.dat | wc -l
 # to count total number of words
 wc -w data.dat | grep -o "[0-9]*"
 # to count the occurence of words starting with a given string
-grep -E "\bmol\w" data.dat | wc -l
+grep -E "\bmol" data.dat | wc -l
