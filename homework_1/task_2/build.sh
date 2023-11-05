bab2d2c60de4629a4526baacc886cf6f8ada6439
Ekansh Sharma <ekanshh.sharma@gmail.com>
2023-11-05 15:40:55 +0100

    [homework_1] Refactor task_1 build script

--- homework_1/task_2/build.sh
+++ homework_1/task_2/build.sh
@@ -1,27 +1,38 @@
 #!/usr/bin/env bash
 
-SRC_DIR="./src"
-INCLUDE_DIR="./include"
-BUILD_DIR="./build"
-RESULTS_DIR="./results"
-
 echo "Building process started..."
-mkdir -p "$BUILD_DIR" "$RESULTS_DIR/lib" "$RESULTS_DIR/bin"
 
-echo "...creating source objects..."
+SRC_DIR=./src
+BUILD_DIR=./build
+INCLUDE_DIR=./include
+RESULTS_DIR=./results
+
+# Check if the build directory exists
+if [ ! -d "$BUILD_DIR" ]; then
+    echo "..create build dir.."
+    mkdir -p "$BUILD_DIR"
+fi
+
+echo "..compile source files into object files.."
 CXXFLAGS="-std=c++17 -I$INCLUDE_DIR"
 c++ $CXXFLAGS -c "$SRC_DIR/sum.cpp" -o "$BUILD_DIR/sum.o"
 c++ $CXXFLAGS -c "$SRC_DIR/subtract.cpp" -o "$BUILD_DIR/subtract.o"
 c++ $CXXFLAGS -c "$SRC_DIR/main.cpp" -o "$BUILD_DIR/main.o"
 
-echo "...creating library..."
+echo "..create static library.."
 ar rcs "$BUILD_DIR/libipb_arithmetic.a" "$BUILD_DIR/subtract.o" "$BUILD_DIR/sum.o"
 
-echo "...compiling main program..."
+echo "..link and create main executable.."
 c++ $CXXFLAGS "$SRC_DIR/main.cpp" -L "$BUILD_DIR" -lipb_arithmetic -o "$BUILD_DIR/test_ipb_arithmetic"
 
-echo "...copying files..."
-cp "$BUILD_DIR/libipb_arithmetic.a" "$RESULTS_DIR/lib/"
-cp "$BUILD_DIR/test_ipb_arithmetic"  "$RESULTS_DIR/bin/"
+# Check if the build directory exists
+if [ ! -d "$RESULTS_DIR" ]; then
+    echo "..create results dir.."
+    mkdir -p "$RESULTS_DIR/bin" "$RESULTS_DIR/lib"
+fi
+
+echo "..copying executable and libraries to results dir.."
+cp -p "$BUILD_DIR/libipb_arithmetic.a" "$RESULTS_DIR/lib/"
+cp -p "$BUILD_DIR/test_ipb_arithmetic"  "$RESULTS_DIR/bin/"
 
 echo "Build process completed."
