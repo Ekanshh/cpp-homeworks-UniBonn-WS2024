d0f2d78c0d248f085d65c59947eeed8d27093269
Ekansh Sharma <ekanshh.sharma@gmail.com>
2023-12-06 00:10:36 +0100

    [homework_4] Code Refactoring to fix issue #3 - Declare template and its attributes directly in header-file, making it header only library - Use const in func args to avoid unwanted modifications - Use const in func accessing information from the custom struct

--- homework_4/core/ipb_container.hpp
+++ homework_4/core/ipb_container.hpp
@@ -1,21 +1,57 @@
 #pragma once
 
 #include <string>
-#include <tuple>
 #include <vector>
 
 namespace ipb {
 
 template <typename T>
+
 struct named_vector {
     std::string type_name;
     std::vector<T> std_vector;
-    std::string name();
-    std::vector<T> vector();
-    std::size_t size();
+
+    std::string name() const;
+    std::vector<T> vector() const;
+    std::size_t size() const;
     bool empty();
-    void resize(int size);
-    int capacity();
-    void reserve(int size);
+    void resize(const int& size);
+    int capacity() const;
+    void reserve(const int& size);
 };
+
+template <typename T>
+std::string named_vector<T>::name() const {
+    return type_name;
+}
+
+template <typename T>
+inline std::vector<T> named_vector<T>::vector() const {
+    return std_vector;
+}
+
+template <typename T>
+std::size_t named_vector<T>::size() const {
+    return std_vector.size() + type_name.size();
+}
+
+template <typename T>
+bool named_vector<T>::empty() {
+    return std_vector.empty() || type_name.empty();
+}
+
+template <typename T>
+void named_vector<T>::resize(const int& size) {
+    std_vector.resize(size);
+}
+
+template <typename T>
+int named_vector<T>::capacity() const {
+    return std_vector.capacity();
+}
+
+template <typename T>
+void named_vector<T>::reserve(const int& size) {
+    std_vector.reserve(size);
+}
 }  // namespace ipb
