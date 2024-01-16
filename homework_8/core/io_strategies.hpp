de8c772de9b1b5fbb5e1b6ff34631b55916fb311
Ekansh Sharma <ekanshh.sharma@gmail.com>
2024-01-16 01:58:21 +0100

    [homework_8] Resolve clangd error to explicitly define virtual destructor for strategies

--- homework_8/core/io_strategies.hpp
+++ homework_8/core/io_strategies.hpp
@@ -13,18 +13,24 @@ struct ImageData {
 
 class IoStrategy {
 public:
+    IoStrategy() = default;
+    virtual ~IoStrategy() = default;
     virtual ImageData ReadFromDisk(const std::string& file_name) const = 0;
     virtual void WriteToDisk(const std::string& file_name, const ImageData& image) const = 0;
 };
 
 class PngIoStrategy : public IoStrategy {
 public:
+    PngIoStrategy() = default;
+    ~PngIoStrategy() override = default;
     ImageData ReadFromDisk(const std::string& file_name) const override;
     void WriteToDisk(const std::string& file_name, const ImageData& image) const override;
 };
 
 class PpmIoStrategy : public IoStrategy {
 public:
+    PpmIoStrategy() = default;
+    ~PpmIoStrategy() override = default;
     ImageData ReadFromDisk(const std::string& file_name) const override;
     void WriteToDisk(const std::string& file_name, const ImageData& image) const override;
 };
